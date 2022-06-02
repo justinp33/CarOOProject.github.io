143265b27ca46f1a4c30a11421b7317bd9327dd9
Justin Passmore <justin_passmore@Justins-MacBook-Pro.local>
2022-06-02 14:26:05 -0400

    Completed Part Four of Project - added Garage class

--- CarOOProject.js
+++ CarOOProject.js
@@ -28,3 +28,20 @@ class Motorcycle extends Vehicle {
         return 'VROOM!!!';
     }
 }
+
+class Garage {
+    constructor(capacity) {
+        this.capacity = capacity;
+        this.vehicles = [];
+    }
+    add(newVehicle) {
+        if(!(newVehicle instanceof Vehicle)) {
+            return 'Only vehicles are allowed in here!';
+        }
+        if(this.vehicles.length >= this.capacity) {
+            return `Sorry, we're full`;
+        }
+        this.vehicles.push(newVehicle);
+        return 'Vehicle added!';
+    }
+} 
